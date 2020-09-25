843c80909ccd722cb3b8b97f958cbca7e9aeca6f
theakaah <theakaah@gmail.com>
2020-09-25 19:13:30 +0900

    errror 없게

--- Prometheus/Prometheus/Form1.cs
+++ Prometheus/Prometheus/Form1.cs
@@ -15,9 +15,9 @@ namespace Prometheus
         public Form1()
         {
             InitializeComponent();
-            public static string[] machine_6 = new string[] { "PH0001", "PH0008", "3", "4", "5" };
-            public static string[] machine_7 = new string[] { "PHT001", "PHT008", "3", "4", "5" };
-            public static string[] machine_8 = new string[] { "PAS001", "PAS008", "31", "4", "5" };
+            string[] machine_6 = new string[] { "PH0001", "PH0008", "3", "4", "5" };
+            string[] machine_7 = new string[] { "PHT001", "PHT008", "3", "4", "5" };
+            string[] machine_8 = new string[] { "PAS001", "PAS008", "31", "4", "5" };
 
 
 
@@ -35,8 +35,8 @@ namespace Prometheus
 
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
-            mirror_machine_sel.Items.RemoveAt(mirror_machine_sel.SelectedIndex);
-            mirror_machine_sel.Items.AddRange(machine_6);
+            //mirror_machine_sel.Items.RemoveAt(mirror_machine_sel.SelectedIndex);
+            //mirror_machine_sel.Items.AddRange(machine_6);
 
 
 
